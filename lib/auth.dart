1f6fbc8ae7947c2ca3327de4cb63d17e92abf817
VARUN-KUMAR13 <22b81a05c3@cvr.ac.in>
2025-02-26 14:25:31 +0530

    First Commit

---
+++ lib/auth.dart
@@ -0,0 +1,54 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:event_management_app/database.dart';
+import 'package:event_management_app/saved_data.dart';
+//import 'package:event_management_app/checkSessions.dart';
+
+Client client = Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('67614326000258668bea')
+    .setSelfSigned(
+        status: true); // For self signed certificates, only use for development
+
+Account account = Account(client);
+// Register User
+Future<String> createUser(String name, String email, String password) async {
+  try {
+    final user = await account.create(
+        userId: ID.unique(), email: email, password: password, name: name);
+    saveUserData(name, email, user.$id);
+    return "success";
+  } on AppwriteException catch (e) {
+    return e.message.toString();
+  }
+}
+
+// Login User
+
+Future loginUser(String email, String password) async {
+  try {
+    final user = 
+    await account.createEmailPasswordSession(email: email, password: password);
+    SavedData.saveUserId(user.userId);
+    getUserData();
+    return true;
+  } on AppwriteException {
+    return false;
+  }
+}
+
+// Logout the user
+Future logoutUser() async {
+  await account.deleteSession(sessionId: 'current');
+  await SavedData.clearSavedData();
+}
+
+// check if user have an active session or not
+
+Future checkSessions() async {
+  try {
+    await account.getSession(sessionId: 'current');
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
